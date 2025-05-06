b41f8f7439c84f09342700ff79d5df478c0b4ac4
Parth <parthd2005@gmail.com>
2025-05-05 20:56:03 -0400

    Initial Commit

---
+++ FocusSense/FocusSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusSenseApp.swift
+//  FocusSense
+//
+//  Created by Parth Dangi on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusSenseTests/FocusSenseTests.swift
@@ -0,0 +1,16 @@
+//
+//  FocusSenseTests.swift
+//  FocusSenseTests
+//
+//  Created by Parth Dangi on 5/5/25.
+//
+
+import Testing
+
+struct FocusSenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
